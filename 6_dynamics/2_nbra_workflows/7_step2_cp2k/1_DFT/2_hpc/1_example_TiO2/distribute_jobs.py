5164a51dd8ca2505ee4f5b89f7f9b26029985eaa
MohammadShakiba <mshakiba.kerman.iran@gmail.com>
2023-05-27 19:33:24 -0400

    First working version

--- 6_dynamics/2_nbra_workflows/7_step2_cp2k/1_DFT/2_hpc/1_example_TiO2/distribute_jobs.py
+++ 6_dynamics/2_nbra_workflows/7_step2_cp2k/1_DFT/2_hpc/1_example_TiO2/distribute_jobs.py
@@ -12,7 +12,7 @@ njobs = 1
 submission_exe = 'sbatch'
 # Removing the previous folders if existed. You can keep them as well 
 # but Libra will overwrite some of the data if their names are the same
-os.system('rm -rf res job* all_*')
+#os.system('rm -rf res job* all_*')
 
 print('Distributing jobs...')
 CP2K_methods.distribute_cp2k_libint_jobs(submit_template, run_python_file, istep, fstep, njobs, run_slurm, submission_exe)
